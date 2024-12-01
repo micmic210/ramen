7850d374634be4816ba1f81620f27e5eed6da17d
Michiko Inoue <inouem888@gmail.com>
2024-12-01 20:31:52 +0000

    Updated Menu model to set a default image for image field

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Menu(models.Model):
     CATEGORY_CHOICES = [
@@ -11,7 +12,12 @@ class Menu(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='ramen')
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+    image = CloudinaryField(
+        'image',
+        folder="menu_images/",
+        use_filename=True,
+        unique_filename=False
+    )
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
